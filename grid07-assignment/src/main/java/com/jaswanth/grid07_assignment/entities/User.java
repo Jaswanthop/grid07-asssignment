7730d3c8e7c781147b3aa3ecb387b0c5f94dca28
Jaswanthop <jaswanth09pasala@gmail.com>
2026-05-06 13:57:51 +0530

    created user entity

---
+++ grid07-assignment/src/main/java/com/jaswanth/grid07_assignment/entities/User.java
@@ -0,0 +1,23 @@
+package com.jaswanth.grid07_assignment.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false,unique = true)
+   private String username;
+
+   @Column(nullable = false,name="is_premimum")
+   private boolean isPremium=false;
+}
