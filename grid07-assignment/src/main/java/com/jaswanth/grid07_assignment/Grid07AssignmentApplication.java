1c376844eeb93273497ce22db9b127d94892f5f1
Jaswanthop <jaswanth09pasala@gmail.com>
2026-05-06 14:10:10 +0530

    Add Spring Boot project files

---
+++ grid07-assignment/src/main/java/com/jaswanth/grid07_assignment/Grid07AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.jaswanth.grid07_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grid07AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grid07AssignmentApplication.class, args);
+	}
+
+}
---
+++ grid07-assignment/src/test/java/com/jaswanth/grid07_assignment/Grid07AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaswanth.grid07_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grid07AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
