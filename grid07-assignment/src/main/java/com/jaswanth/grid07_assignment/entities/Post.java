82ec64e0addcf7459645a941b392448c762c772f
Jaswanthop <jaswanth09pasala@gmail.com>
2026-05-06 15:09:49 +0530

    updated  post entity

--- grid07-assignment/src/main/java/com/jaswanth/grid07_assignment/entities/Post.java
+++ grid07-assignment/src/main/java/com/jaswanth/grid07_assignment/entities/Post.java
@@ -3,6 +3,7 @@ package com.jaswanth.grid07_assignment.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -20,19 +21,25 @@ public class Post {
     private Long id;
 
     @Column(nullable = false)
-    private Long authorId;
-
-    @Column(nullable = false)
     private String content;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false,name="author_type")
     private AuthorType authorType;
 
+    @CreationTimestamp
     @Column(updatable = false,name="created_at")
     private LocalDateTime createdAt;
 
     @Column(name="like_count",nullable = false)
     private long likeCount=0;
 
+    @ManyToOne(fetch = FetchType.LAZY,optional = true)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = true)
+    @JoinColumn(name = "bot_id")
+    private Bot bot;
 }
